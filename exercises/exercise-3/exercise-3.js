c75ccd16d366a96bf25d4aa9b281148945cf7b6e
niamhmadden23 <83446408+niamhmadden23@users.noreply.github.com>
2021-09-24 16:19:03 +0100

    exercise 3 complete

--- exercises/exercise-3/exercise-3.js
+++ exercises/exercise-3/exercise-3.js
@@ -6,3 +6,16 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPrice: 1.0 },
   { itemName: "Hash Brown", quantity: 4, unitPrice: 0.4 },
 ];
+
+const createReceipt = function (arr) {
+  console.log("QTY     ITEM     TOTAL");
+  let total = 0;
+  arr.map((item) => {
+    ({ itemName, quantity, unitPrice } = item);
+    let totalPrice = quantity * unitPrice;
+    console.log(`${quantity}    ${itemName}    ${totalPrice.toFixed(2)} `);
+    total += totalPrice;
+  });
+  console.log(`TOTAL £${total}`);
+};
+createReceipt(order);
